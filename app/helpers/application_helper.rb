bf210165602141cce5b7a5486b02464ea3f47a7a
Mitchell Bryson <198377+mitchellfyi@users.noreply.github.com>
2025-09-27 15:47:15 +0100

    add meta tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,51 @@
 module ApplicationHelper
+
+  # Accessibility helpers
+  def skip_link(target = "#main-content", text = t('a11y.skip_to_content'))
+    link_to text, target, class: "skip-link sr-only focus:not-sr-only focus:absolute focus:top-0 focus:left-0 bg-blue-600 text-white p-2 z-50 focus:z-50"
+  end
+
+
+  # Locale management helpers
+  def current_locale_name
+    I18n.t("locales.#{I18n.locale}", default: I18n.locale.to_s.upcase)
+  end
+
+  def locale_options
+    I18n.available_locales.map do |locale|
+      [I18n.t("locales.#{locale}", default: locale.to_s.upcase), locale]
+    end
+  end
+
+  def rtl_locale?
+    %i[ar he].include?(I18n.locale)
+  end
+
+  # ARIA helpers
+  def aria_label(key, options = {})
+    { 'aria-label': t(key, **options) }
+  end
+
+  def aria_describedby(id)
+    { 'aria-describedby': id }
+  end
+
+  def sr_only(text)
+    content_tag :span, text, class: "sr-only"
+  end
+
+  # Icon with accessibility text
+  def icon_with_text(icon_class, text, options = {})
+    content_tag :span, **options do
+      concat content_tag(:span, "", class: icon_class, 'aria-hidden': true)
+      concat sr_only(text)
+    end
+  end
+
+  # Form helpers with better accessibility
+  def accessible_form_with(model: nil, **options, &block)
+    options[:role] = 'form' unless options.key?(:role)
+    form_with(model: model, **options, &block)
+  end
+
 end
---
+++ config/initializers/meta_tags.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'meta-tags'
+
+# MetaTags configuration
+MetaTags.configure do |config|
+  # How many characters should the title be truncated to?
+  config.title_limit = 70
+
+  # Maximum length of the description tag
+  config.description_limit = 160
+
+  # Maximum length of the keywords tag
+  config.keywords_limit = 255
+
+  # Default separator for keywords
+  config.keywords_separator = ", "
+
+  # When true, keywords will be converted to lowercase, otherwise they will appear on the page as provided
+  config.keywords_lowercase = true
+
+  # When true, the output will not include new line characters between meta tags
+  config.minify_output = true
+end
