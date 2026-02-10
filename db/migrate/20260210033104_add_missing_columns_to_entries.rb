1ed4f05cb58cdb5706e5fdcf33346481c57f0502
Mitchell <198377+mitchellfyi@users.noreply.github.com>
2026-02-10 16:59:16 +0000

    style: Fix array bracket spacing for RuboCop

--- db/migrate/20260210033104_add_missing_columns_to_entries.rb
+++ db/migrate/20260210033104_add_missing_columns_to_entries.rb
@@ -13,8 +13,8 @@ class AddMissingColumnsToEntries < ActiveRecord::Migration[8.1]
       add_column :entries, :ai_tags, :jsonb, default: {}, null: false
     end
 
-    unless index_exists?(:entries, [:site_id, :listing_type], name: "index_entries_on_site_id_and_listing_type")
-      add_index :entries, [:site_id, :listing_type], name: "index_entries_on_site_id_and_listing_type"
+    unless index_exists?(:entries, [ :site_id, :listing_type ], name: "index_entries_on_site_id_and_listing_type")
+      add_index :entries, [ :site_id, :listing_type ], name: "index_entries_on_site_id_and_listing_type"
     end
 
     remove_index :affiliate_clicks, name: "index_affiliate_clicks_on_listing_clicked", if_exists: true
